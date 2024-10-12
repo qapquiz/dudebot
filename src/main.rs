5d356891796ac678f2b276cbb772f6b476923598
qapquiz <arm.armariya@gmail.com>
2024-10-12 19:13:01 +0700

    feat: add reaction to verify

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 mod commands;
 
 use dotenv::dotenv;
-use poise::serenity_prelude as serenity;
+use poise::{
+    framework,
+    serenity_prelude::{self as serenity, futures::task::waker, CacheHttp, MessageId},
+};
 use std::env::var;
 
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
-pub struct Data {}
+pub struct Data {
+    verifed_message_id: MessageId,
+}
 
 async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
     match error {
@@ -29,21 +34,20 @@ async fn main() {
     dotenv().ok();
 
     let options = poise::FrameworkOptions {
-        commands: vec![
-            commands::create_verify_message(),
-            commands::verify(),
-        ],
+        commands: vec![commands::create_verify_message(), commands::verify()],
         prefix_options: poise::PrefixFrameworkOptions {
             prefix: Some("!".into()),
             ..Default::default()
         },
-        command_check: Some(|ctx| Box::pin(async move {
-            if ctx.command().name == "verify" {
-                return Ok(true)
-            }
+        command_check: Some(|ctx| {
+            Box::pin(async move {
+                if ctx.command().name == "verify" {
+                    return Ok(true);
+                }
 
-            Ok(false)
-        })),
+                Ok(false)
+            })
+        }),
         skip_checks_for_owners: true,
         pre_command: |ctx| {
             Box::pin(async move {
@@ -55,6 +59,9 @@ async fn main() {
                 println!("Executed command {}!", ctx.command().qualified_name);
             })
         },
+        event_handler: |ctx, event, framework, data| {
+            Box::pin(event_handler(ctx, event, framework, data))
+        },
         on_error: |error| Box::pin(on_error(error)),
         ..Default::default()
     };
@@ -65,7 +72,9 @@ async fn main() {
                 println!("Logged in as {}", _ready.user.name);
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
 
-                Ok(Data {})
+                Ok(Data {
+                    verifed_message_id: MessageId::new(1028609208989523969_u64),
+                })
             })
         })
         .options(options)
@@ -81,4 +90,32 @@ async fn main() {
         .await;
 
     client.unwrap().start().await.unwrap()
+}
+
+async fn event_handler(
+    ctx: &serenity::Context,
+    event: &serenity::FullEvent,
+    _framework: poise::FrameworkContext<'_, Data, Error>,
+    data: &Data
+) -> Result<(), Error> {
+    if let serenity::FullEvent::ReactionAdd { add_reaction } = event {
+        if add_reaction.message_id == data.verifed_message_id
+            && add_reaction.emoji.unicode_eq("✅")
+        {
+            let guild_id = add_reaction.guild_id.unwrap();
+            let roles = guild_id.roles(&ctx.http()).await.unwrap();
+            let verified_role = roles.values().find(|role| role.name == "verified").unwrap();
+
+            add_reaction
+                .member
+                .clone()
+                .unwrap()
+                .add_role(&ctx.http(), verified_role.id)
+                .await?;
+
+            println!("Verified member via ReactionAdd");
+        }
+    }
+
+    Ok(())
 }
